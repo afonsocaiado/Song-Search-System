cc9612e7ee560794ace95cc306c916b2dbe1ec34
afonsocaiado <afonsocaiado1@gmail.com>
2022-01-17 01:49:02 +0000

    Make fix, fix to unnamed column added

--- src/combine.py
+++ src/combine.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
     file_extension = '.csv'
     all_filenames = [i for i in glob.glob(f"../dataset/Song-Lyrics-Dataset/csv/*{file_extension}")]
     
-    combinedLyrics = pd.concat([pd.read_csv(f) for f in all_filenames])  
+    combinedLyrics = pd.concat([pd.read_csv(f, index_col=[0]) for f in all_filenames])  
     
     #Reading spotify playlist dataset
-    spotifyTest = pd.read_csv('spotifyData.csv')
+    spotifyTest = pd.read_csv('spotifyData.csv', index_col=[0])
 
     #Combine both
     merged = pd.merge(combinedLyrics, spotifyTest, on=['Artist', 'Title'])
--- src/extend.py
+++ src/extend.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     
     final['id'] = range(1, len(final) + 1)
     
-    final.to_csv('music_lyrics.csv', index=False)
+    final.to_csv('../solr/data/music_lyrics.csv', index=False)
     
     print('Extended the data and wrote it to music_lyrics.csv\n')
